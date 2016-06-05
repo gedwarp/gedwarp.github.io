44126ed025f51cc8ecc4a98739ff0c46cafcac4b
gedwarp <gedwarp@gmail.com>
2016-06-05 11:17:19 +0900

    react
    react

---
+++ js/contents/career.js
@@ -0,0 +1,110 @@
+const career={
+  profile : "Technical Culture Transform Enabler with 10+ year’s success devising innovative and tailored  solution to meet ever-changing business needs.",
+  experiences : [{
+     jobTitle : "General Manager",
+     during : "2012 - Present" ,
+     company : "Samsung Electronics S/W R&D Center, Seoul",
+     detail :"Strategic Planning, Change Artist, Lean & Agile Coach, Project/Product  Management, Creative Facilitator, Conference/Hackathon Organizer",
+     location :"Seoul, South Korea"
+  },{
+     jobTitle : "Software Quality Lead Engineer",
+     during : "2007 - 2012" ,
+     company : "Samsung Electronics, Suwon",
+     detail :"Policy, Planning, Test Case Design, Automation, Training, Process Management, Android Testing",
+     location :"Suwon, South Korea"
+  },{
+     jobTitle : "Board Member",
+     during : "2005 - Present" ,
+     company : "ISTQB Korea",
+     detail :"Advisor",
+     location :"Seoul, South Korea"
+  },{
+     jobTitle : "SW Quality Lead Engineer",
+     during : "2004 - 2005" ,
+     company : "I-ON Communications",
+     detail :"Quality Team and Process building, Test Planning, Execution, Report",
+     location :"Tokyo, Japan"
+  },{
+     jobTitle : "Software Developer",
+     during : "2000 - 2004" ,
+     company : "I-ON Communications",
+     detail :"Java, Python, PHP Developer",
+     location :"Seoul, South Korea"
+  }],
+  project :{
+    description :"list of projects",
+    list : [{
+      title : "Bleuhack",
+      url : "#",
+      role : "Founder",
+      during :"2014 - Present",
+      detail :"Hackathon in Samsung group",
+      location :"South Korea"
+    },{
+      title : "Tizen Developer Conference",
+      url : "#",
+      during :"2013",
+      role : "Organizer",
+      detail :"Conference Organizer",
+      location :"San Francisco, USA"
+    },{
+      title : "Lean Startup Conference Live Stream Event",
+      url : "#",
+      during :"2012-2014",
+      role : "Organizer",
+      detail :"Event Organizer",
+      location :"Seoul, South Korea"
+    },{
+      title : "SXSW",
+      url : "#",
+      during :"2013",
+      role : "Speaker",
+      detail :"SXSW Edu Speaker",
+      location :"Austin, USA"
+    },{
+      title : "Agile Testing Days",
+      url : "#",
+      during :"2010",
+      role : "Speaker",
+      detail :"Agile Testing Speaker",
+      location :"Berlin, German"
+    }]
+  },
+  opensource :{
+    description :"list of open source projects",
+    list : [{
+      title : "Tizen",
+      url : "#",
+      role : "staff,Organizer",
+      during :"2013",
+      detail :"As a Organizer,successfully Conference to be done",
+      location :""
+    }]
+  },
+  skills : [{
+    skill : "Project Management",
+    level : "98%"
+  },{
+    skill : "Agile",
+    level :"90%"
+  },{
+    skill : "opensource Governance",
+    level :"98%"
+  },{
+    skill : "Software Quality Engineering",
+    level :"100%"
+  },{
+    skill : "Business Development",
+    level :"90%"
+  },{
+    skill : "Facilitation",
+    level :"100%"
+  },{
+    skill : "Conference Organization",
+    level :"100%"
+  },{
+    skill : "javascript",
+    level :"80%"
+  }]
+}
+module.exports= career;
---
+++ js/contents/info.js
@@ -0,0 +1,25 @@
+const userinfo={
+  profile:{
+    name : "Hyungil CHO",
+    tag : "Transform Enabler",
+    mail : "gedwarp@gmail.com",
+    phone : "gedwarp@gmail.com",
+    blog : "gedwarp.github.io",
+    linkedin : "linkedin.com/in/gedwarp",
+    github : "github.com/gedwarp",
+    twitter : "@gedwarp",
+    imgUrl : "http://i.imgur.com/9JG3lf3.png"
+  },
+  educations : [{
+     major : "The Master of Business Administration",
+     school : "Kelly school of Business, Indiana University",
+     during : "2016-2017"
+  },{
+     major : "BSc in Computer educations",
+     school : "SKKU University",
+     during : "1998-2007"
+  }],
+  languages : [{lang:"Korean",fluency:"Native"},{lang:"English",fluency:"Professional"},{lang:"Japanese/Mandarin Chinese",fluency:"Conversational"}],
+  interests :["Startup Samsung","Social Impact","Full Stack"]
+}
+module.exports= userinfo;
